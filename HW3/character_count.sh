f64e5a93c129526eb5a2b67bb01de713368167ab
pujin2 <pujin2@illinois.edu>
2018-03-15 18:04:21 -0500

    Add HW3

---
+++ HW3/character_count.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+###############################################################################
+# Instructions:
+###############
+#
+# Modify this script so that it prints the number of characters
+# in the file /usr/share/emacs/24.3/etc/WHY-FREE (and nothing else).
+#
+# IMPORTANT NOTE: Your script should ONLY print the number of characters.
+#                 It should NOT also print the file name.
+#
+#
+# HINT: You will likely need to use either a pipe or a redirect operator
+#
+###############################################################################\
+cat /usr/share/emacs/24.3/etc/WHY-FREE | wc -m
